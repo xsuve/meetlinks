6094ccfe88fa475b122738c3f18f2fce262236e9
xsuve <georgebaba99@yahoo.com>
2021-03-24 20:13:48 +0200

    First commit

---
+++ popup.js
@@ -0,0 +1,23 @@
+chrome.storage.sync.get('links', function(response) {
+  if(response.links.length > 0) {
+    document.getElementById('data').value = JSON.stringify(response.links, null, 2);
+  }
+});
+
+var updateBtn = document.getElementById('update');
+updateBtn.addEventListener('click', async () => {
+  var json = document.getElementById('data').value;
+  var links = JSON.parse(json);
+
+  chrome.storage.sync.set({ links: links }, function(response) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      chrome.tabs.update(tabs[0].id, { url: tabs[0].url });
+      updateBtn.innerHTML = 'Saved!';
+      updateBtn.style.backgroundColor = '#5cc67d';
+      setTimeout(function() {
+        updateBtn.innerHTML = 'Update';
+        updateBtn.style.backgroundColor = '#001eff';
+      }, 2000);
+    });
+  });
+});
